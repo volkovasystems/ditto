c335dd0804ea40001c00132d1cb97093a7e40323
volkovasystems <richeve.bebedor@gmail.com>
2014-03-13 09:10:09 +0000

    Updated with modified files.

--- ditto.js
+++ ditto.js
@@ -4,6 +4,8 @@
       "configurable": true,
       "get": function get( ){
         var currentVersionMethod;
+        var methodList = { };
+        registerInterval( "module-update:" + module.name + "." + property )
         //[UPDATE PROCEDURES HERE]
         return currentVersionMethod;
       }
@@ -19,3 +21,39 @@
     2. Reinsert the new version of the function if there are updates
       on the tracked object.
 */
+/*:
+    @include:
+        {
+            
+        }
+    @end-include
+*/
+ditto = function ditto( module, updateURI ){
+    var property;
+    //Check if the module is class by naming convention.
+    if( ( /^[A-Z]/ ).test( module.name ) ){
+        //This is a class.
+    }else{
+        /*
+            All modules not a class should either register
+                through window or global property.
+        */
+        property = module;
+        switch( environment ){
+            case "browser":
+                window.name = "window";
+                module = window;
+                break;
+                
+            case "nodejs":
+                global.name = "global";
+                module = global;
+                break;
+                
+            default:
+                throw new Error( "cannot determine environment" );
+        }
+        
+        
+    }
+};
